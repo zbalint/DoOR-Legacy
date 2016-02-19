ffff65697346c538d8ac371d36d394177b4b4984
Bálint Zoltán Márton <balint.zoltan.marton@gmail.com>
2016-02-19 19:02:59 +0100

    Added show version function

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("%s", "Hello World!");
+void showVersion() {
+    printf("Version: %s.%s.%s\n", VERSION, COMMIT_NUMBER, GIT_SHA1);
+}
+
+void showUsage() {
+    printf("Usage: door -v | --version     : print version\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        if ((strcmp(argv[1], "-v") == 0) || (strcmp(argv[1], "--version") == 0)) {
+            showVersion();
+        } else {
+            showUsage();
+        }
+    } else {
+        showUsage();
+    }
     return 0;
 }
